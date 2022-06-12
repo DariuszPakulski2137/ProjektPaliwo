9fcf2b665f5b5e548d5717cff77497234ca16950
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2022-06-12 16:26:05 +0200

    Pusty projekt
    Dodanie początkowego projektu z IntelliJ ze skonfigurowanym springbootem.

---
+++ src/main/java/com/projekt/PaliwoApp/PaliwoAppApplication.java
@@ -0,0 +1,15 @@
+package com.projekt.PaliwoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class PaliwoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaliwoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt/PaliwoApp/PaliwoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.PaliwoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaliwoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
