4c084af37d8b1bf7a733422d3133c5454aafcc15
Keimilo <83248171+Keimilo@users.noreply.github.com>
2022-06-21 21:54:29 +0200

    Stworzenie klasy user
    Stworzenie i dodanie klasy związanej z modelem użytkownika

---
+++ src/main/java/com/projekt/PaliwoApp/model/User.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Receipt> receiptList = new ArrayList<>();
+
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public List<Receipt> getReceiptList() {
+        return receiptList;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPassword(String pass) {
+        this.password = pass;
+    }
+}
